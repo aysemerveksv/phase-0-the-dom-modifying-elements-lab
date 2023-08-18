d054e6d778af6c469de0e4db3fd3d6b918dcda62
Ayşe Merve Kosova <mervekosova1@hotmail.com>
2023-08-18 17:27:08 +0300

    Complete DOM Modification Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+// Create DOM Elements Programmatically
+const mainElement = document.querySelector("main#main");
+if (mainElement) {
+  mainElement.remove();
+}
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Ayşe is the champion";
+document.body.appendChild(newHeader);
